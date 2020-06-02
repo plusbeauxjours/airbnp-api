885660a5f3308e378e05d7ca8c6feb07e98ad6d3
plusbeauxjours <plusbeauxjours@gmail.com>
2020-06-02 18:59:15 +0700

    @api_view

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from . import models
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(["GET", "POST", "PUT", "DELETE"])
+def list_rooms(request):
+    print(models.Room.objects.all())
+    return Response()
