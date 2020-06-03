578f5019166fb0557251c34395516fccf8b20804
plusbeauxjours <plusbeauxjours@gmail.com>
2020-06-04 01:04:42 +0700

    RoomView CBV

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,18 +4,6 @@ from . import views
 app_name = "rooms"
 
 urlpatterns = [
-    path("", views.room_view),
-    # path("list/", views.room_list),
-    # path("list/", views.ReadRoomListView.as_view()),
-    # path("<int:pk>/", views.ReadRoomDetailView.as_view()),
-    # path("<int:pk>/", views.RoomDetailView.as_view()),
-    # path("<uuid:uuid>/", views.ReadRoomDetailView.as_view()),
+    path("", views.RoomsView.as_view()),
+    path("<uuid:uuid>/", views.RoomView.as_view()),
 ]
-
-# from rest_framework.routers import DefaultRouter
-# from . import viewsets
-
-# router = DefaultRouter()
-# router.register("", viewsets.RoomViewset, basename="room")
-
-# urlpatterns = router.urls
--- rooms/views.py
+++ rooms/views.py
@@ -1,29 +1,16 @@
 from . import models, serializers
 from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-
-# from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 
-# class ReadRoomListView(ListAPIView):
-#     queryset = models.Room.objects.all()
-#     serializer_class = serializers.ReadRoomSerializer
-
-
-# class ReadRoomDetailView(RetrieveAPIView):
-#     queryset = models.Room.objects.all()
-#     serializer_class = serializers.ReadRoomSerializer
-#     lookup_field = "uuid"
-
-
-@api_view(["GET", "POST"])
-def room_view(request):
-    if request.method == "GET":
+class RoomsView(APIView):
+    def get(self, request):
         rooms = models.Room.objects.all()[:5]
         serializer = serializers.ReadRoomSerializer(rooms, many=True).data
         return Response(serializer)
-    elif request.method == "POST":
+
+    def post(self, request):
         if not request.user.is_authenticated:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         serializer = serializers.WriteRoomSerializer(data=request.data)
@@ -32,4 +19,20 @@ def room_view(request):
             room_serializer = serializers.ReadRoomSerializer(room).data
             return Response(data=room_serializer, status=status.HTTP_200_OK)
         else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class RoomView(APIView):
+    def get(self, request, uuid):
+        try:
+            room = models.Room.objects.get(uuid=uuid)
+            serializer = serializers.ReadRoomSerializer(room).data
+            return Response(data=serializer)
+        except models.Room.DoesNotExist:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
