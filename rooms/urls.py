64dd355332d3263ee096dc22a0c640b230d7d231
plusbeauxjours <plusbeauxjours@gmail.com>
2020-06-03 12:22:45 +0700

    APIView & ListAPIView & Pagination

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 app_name = "rooms"
 
-urlpatterns = [path("list/", views.list_rooms)]
+urlpatterns = [path("list/", views.ListRoomsView.as_view())]
+# urlpatterns = [path("list/", views.list_rooms)]
--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
-from . import models
-from . import serializers
+from . import models, serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-@api_view(["GET"])
-def list_rooms(request):
-    rooms = models.Room.objects.all()
-    serialized_rooms = serializers.RoomSerializer(rooms, many=True)
-    return Response(data=serialized_rooms.data)
+# @api_view(["GET"])
+# def list_rooms(request):
+#     rooms = models.Room.objects.all()
+#     serialized_rooms = serializers.RoomSerializer(rooms, many=True)
+#     return Response(serialized_rooms.data)
+
+from rest_framework.views import APIView
+
+
+# class ListRoomsView(APIView):
+#     def get(self, request):
+#         rooms = models.Room.objects.all()
+#         serializer = serializers.RoomSerializer(rooms, many=True)
+#         return Response(serializer.data)
+
+from rest_framework.generics import ListAPIView
+
+
+class ListRoomsView(ListAPIView):
+    queryset = models.Room.objects.all()
+    serializer_class = serializers.RoomSerializer
