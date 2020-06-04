904ec96df3d04881fa1c2b15a30785bc96b1dfa9
plusbeauxjours <plusbeauxjours@gmail.com>
2020-06-04 12:17:34 +0700

    editRoom

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,8 +1,5 @@
-
 from rest_framework import serializers
-
 from users.serializers import UserSerializer
-
 from .models import Room
 
 
@@ -18,7 +15,7 @@ class ReadRoomSerializer(serializers.ModelSerializer):
 class WriteRoomSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=140)
     address = serializers.CharField(max_length=140)
-    price = serializers.IntegerField(help_text="USD per night")
+    price = serializers.IntegerField()
     beds = serializers.IntegerField(default=1)
     lat = serializers.DecimalField(max_digits=10, decimal_places=6)
     lng = serializers.DecimalField(max_digits=10, decimal_places=6)
@@ -32,12 +29,31 @@ class WriteRoomSerializer(serializers.Serializer):
         return Room.objects.create(**validated_data)
 
     def validate(self, data):
-        if not self.instance:
-            check_in = data.get('check_in')
-            check_out = data.get('check_out')
-            if check_in == check_out:
-                raise serializers.ValidationError("Not enough time between changes")
+        if self.instance:
+            check_in = data.get("check_in", self.instance.check_in)
+            check_out = data.get("check_out", self.instance.check_out)
+            print(check_in, check_out)
+        else:
+            check_in = data.get("check_in")
+            check_out = data.get("check_out")
+            print(check_in, check_out)
+        if check_in == check_out:
+            raise serializers.ValidationError("Not enough time between changes")
         return data
 
     def update(self, instance, validated_data):
-        print(instance, validated_data)
+        instance.name = validated_data.get("name", instance.name)
+        instance.address = validated_data.get("address", instance.address)
+        instance.price = validated_data.get("price", instance.price)
+        instance.beds = validated_data.get("beds", instance.beds)
+        instance.lat = validated_data.get("lat", instance.lat)
+        instance.lng = validated_data.get("lng", instance.lng)
+        instance.bedrooms = validated_data.get("bedrooms", instance.bedrooms)
+        instance.bathrooms = validated_data.get("bathrooms", instance.bathrooms)
+        instance.check_in = validated_data.get("check_in", instance.check_in)
+        instance.check_out = validated_data.get("check_out", instance.check_out)
+        instance.instant_book = validated_data.get(
+            "instant_book", instance.instant_book
+        )
+        instance.save()
+        return instance
--- rooms/views.py
+++ rooms/views.py
@@ -39,6 +39,24 @@ class RoomView(APIView):
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
+    def put(self, request, uuid):
+        room = self.get_room(uuid)
+        if room is not None:
+            if room.user != request.user:
+                return Response(status=status.HTTP_403_FORBIDDEN)
+            serializer = serializers.WriteRoomSerializer(
+                room, data=request.data, partial=True
+            )
+            if serializer.is_valid():
+                room = serializer.save()
+                room_serializer = serializers.ReadRoomSerializer(room).data
+                return Response(data=room_serializer, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response()
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
     def delete(self, request, uuid):
         room = self.get_room(uuid)
         if room is not None:
