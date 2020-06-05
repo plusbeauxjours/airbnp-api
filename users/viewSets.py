e39ace8d96f7feee467e91474b579e9679044228
plusbeauxjours <plusbeauxjours@gmail.com>
2020-06-06 01:26:35 +0700

    **userViewSet Login

--- users/viewSets.py
+++ users/viewSets.py
@@ -1,6 +1,12 @@
+import jwt
+
 from .models import User
 from .serializers import UserSerializer
 from .permissions import IsSelf
+
+from django.conf import settings
+from django.contrib.auth import authenticate
+
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import permissions, status
 from rest_framework.response import Response
@@ -30,3 +36,18 @@ class UserViewSet(ModelViewSet):
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         serializer = UserSerializer(request.user).data
         return Response(data=serializer, status=status.HTTP_200_OK)
+
+    @action(detail=False, metod=["post"])
+    def login(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            encoded_jwt = jwt.encode(
+                {"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256"
+            )
+            return Response(data={"token": encoded_jwt, "id": user.pk})
+        else:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 import jwt
-from django.conf import settings
-from django.contrib.auth import authenticate
+
 from .models import User
+from .serializers import UserSerializer
 from rooms.models import Room
 from rooms.serializers import RoomSerializer
-from .serializers import UserSerializer
+
+from django.conf import settings
+from django.contrib.auth import authenticate
+
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.views import APIView
@@ -85,14 +88,12 @@ class FavsView(APIView):
 
 class Login(APIView):
     def post(self, request):
-        print("user")
         username = request.data.get("username")
         password = request.data.get("password")
         if not username or not password:
             return Response(status=status.HTTP_400_BAD_REQUEST)
         user = authenticate(username=username, password=password)
         if user is not None:
-            print(user.uuid)
             encoded_jwt = jwt.encode(
                 {"uuid": str(user.uuid)}, settings.SECRET_KEY, algorithm="HS256"
             )
