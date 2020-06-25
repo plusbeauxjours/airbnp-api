54827cb3b911ad64c7688780d92173972dfdff40
plusbeauxjours <plusbeauxjours@gmail.com>
2020-06-25 09:40:43 +0700

    receiver User model

--- users/models.py
+++ users/models.py
@@ -34,53 +34,53 @@ class User(AbstractUser, CoreModel):
         ordering = ["-created_at"]
 
 
-# @receiver(post_save, sender=User)
-# def do_something_when_user_created(sender, instance, created, **kwargs):
-#     if created:
-#         user = instance
-#         rooms = user.rooms.all()
-#         if rooms.count() < 5:
-#             room_seeder = Seed.seeder()
-#             room_seeder.add_entity(
-#                 Room,
-#                 3,
-#                 {
-#                     "uuid": lambda x: uuid.uuid4(),
-#                     "user": user,
-#                     "name": lambda x: room_seeder.faker.street_address(),
-#                     "lat": lambda x: random.uniform(40.706943, 40.822943),
-#                     "lng": lambda x: random.uniform(-73.923917, -74.040000),
-#                     "price": lambda x: random.randint(60, 300),
-#                     "beds": lambda x: random.randint(0, 8),
-#                     "bedrooms": lambda x: random.randint(0, 5),
-#                     "bathrooms": lambda x: random.randint(0, 5),
-#                     "instant_book": lambda x: random.choice([True, False]),
-#                     "check_in": lambda x: datetime.now(),
-#                     "check_out": lambda x: datetime.now(),
-#                 },
-#             )
-#             room_seeder.execute()
+@receiver(post_save, sender=User)
+def do_something_when_user_created(sender, instance, created, **kwargs):
+    if created:
+        user = instance
+        rooms = user.rooms.all()
+        if rooms.count() < 5:
+            room_seeder = Seed.seeder()
+            room_seeder.add_entity(
+                Room,
+                3,
+                {
+                    "uuid": lambda x: uuid.uuid4(),
+                    "user": user,
+                    "name": lambda x: room_seeder.faker.street_address(),
+                    "lat": lambda x: random.uniform(40.706943, 40.822943),
+                    "lng": lambda x: random.uniform(-73.923917, -74.040000),
+                    "price": lambda x: random.randint(60, 300),
+                    "beds": lambda x: random.randint(0, 8),
+                    "bedrooms": lambda x: random.randint(0, 5),
+                    "bathrooms": lambda x: random.randint(0, 5),
+                    "instant_book": lambda x: random.choice([True, False]),
+                    "check_in": lambda x: datetime.now(),
+                    "check_out": lambda x: datetime.now(),
+                },
+            )
+            room_seeder.execute()
 
-#             review_seeder = Seed.seeder()
-#             users = User.objects.all()
-#             rooms = user.rooms.all()
-#             review_seeder.add_entity(
-#                 Review,
-#                 20,
-#                 {
-#                     "uuid": lambda x: uuid.uuid4(),
-#                     "user": lambda x: random.choice(users),
-#                     "text": lambda x: review_seeder.faker.text(),
-#                     "room": lambda x: random.choice(rooms),
-#                 },
-#             )
-#             review_seeder.execute()
+            review_seeder = Seed.seeder()
+            users = User.objects.all()
+            rooms = user.rooms.all()
+            review_seeder.add_entity(
+                Review,
+                20,
+                {
+                    "uuid": lambda x: uuid.uuid4(),
+                    "user": lambda x: random.choice(users),
+                    "text": lambda x: review_seeder.faker.text(),
+                    "room": lambda x: random.choice(rooms),
+                },
+            )
+            review_seeder.execute()
 
-#             rooms = user.rooms.all()
-#             for room in rooms:
-#                 for i in range(random.randint(5, 10)):
-#                     Photo.objects.create(
-#                         caption=room_seeder.faker.sentence(),
-#                         room=room,
-#                         file=f"room_photos/{random.randint(1, 50)}.jpeg",
-#                     )
+            rooms = user.rooms.all()
+            for room in rooms:
+                for i in range(random.randint(5, 10)):
+                    Photo.objects.create(
+                        caption=room_seeder.faker.sentence(),
+                        room=room,
+                        file=f"room_photos/{random.randint(1, 50)}.jpeg",
+                    )
